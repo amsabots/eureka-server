3b7b063dd3a6f29477e9ecb7eeaf620e47d83a8c
amsabots <amsabots@gmail.com>
2022-01-08 22:30:51 +0300

    docker file build fix

--- src/main/java/com/abcthebank/interswitch/eurakaserver/EurakaServerApplication.java
+++ src/main/java/com/abcthebank/interswitch/eurakaserver/EurakaServerApplication.java
@@ -3,10 +3,11 @@ package com.abcthebank.interswitch.eurakaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 public class EurakaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurakaServerApplication.class, args);
